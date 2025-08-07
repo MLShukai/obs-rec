fd6b08f5cea7391f9e937db46df5ed2ade33c10c
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-08-07 15:32:33 +0900

    setup project.

--- tests/test_package.py
+++ tests/test_package.py
@@ -3,7 +3,7 @@
 import tomllib
 from pathlib import Path
 
-import python_uv_template
+import obs_rec
 
 PROJECT_ROOT = Path(__file__).parent.parent
 
@@ -12,4 +12,4 @@ def test_version():
     with open(PROJECT_ROOT / "pyproject.toml", "rb") as f:
         pyproject = tomllib.load(f)
 
-    assert python_uv_template.__version__ == pyproject["project"]["version"]
+    assert obs_rec.__version__ == pyproject["project"]["version"]
